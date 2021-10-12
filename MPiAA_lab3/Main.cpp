f763c20824026ac2c2bb9492fcf35c5fc42a1215
Semyon <s_se@ngs.ru>
2021-10-12 18:25:06 +0700

    Добавьте файлы проекта.

---
+++ MPiAA_lab3/Main.cpp
@@ -0,0 +1,15 @@
+#include "mpi.h"
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	int rank, size;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+	std::cout << "Rank is: " << rank << ", size is: " << size << std::endl;
+
+	MPI_Finalize();
+	return 0;
+}
